d76fef2be459960550883ddc86f35eb72e6ce6e6
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2024-03-05 02:18:32 +0530

    Add user controller and user route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,4 +18,4 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000 ");
 });
 
-
+app.use("/api/user", userRoutes );
