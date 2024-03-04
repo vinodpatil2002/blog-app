48856f6f4f3cd88aef251d3bb0fcd7fa395c3eb6
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2024-03-05 02:02:35 +0530

    Add MongoDB connection and dotenv configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB ");
+}
+).catch((err) => {
+    console.log("Error: ", err);
+});
 
 const app = express();
 
