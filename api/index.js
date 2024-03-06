6cfe46840eb84e23aafeb4d0a96561c4d0a03d32
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2024-03-06 23:59:09 +0530

    Add updateUser API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.listen(3000, () => {
