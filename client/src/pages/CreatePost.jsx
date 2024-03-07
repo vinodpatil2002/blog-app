c2f0e9d7e456b7bba55f406b986f8492f66cae6f
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2024-03-07 18:59:06 +0530

    Complete create post functionality in frontend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -7,19 +7,23 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 // import { set } from 'mongoose';
 import {CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import {useNavigate} from 'react-router-dom';
 
 export default function CreatePost() {
     const [files , setFiles] = useState(null);
+    const navigate = useNavigate();
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [imageUrl, setImageUrl] = useState(null);
+    const [publishError, setPublishError] = useState(null);
     const [formData, setFormData] = useState({});
+
     const handleUploadImage = async () => {
         try {
             if(!files) {
                 setImageUploadError('Please select an image to upload');
                 return;
-            };
+            }
             setImageUploadError(null);
             const storage = getStorage(app);
             const fileName = new Date().getTime() + files.name;
@@ -48,13 +52,40 @@ export default function CreatePost() {
             console.log(error);
         }
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+            if(!res.ok) {
+                setPublishError(data.message);
+                return;
+            }
+            if(res.ok) {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`)
+            }
+        } catch (error) {
+            setPublishError('Failed to create post');
+        }
+    }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post </h1>
-        <form className='gap-4 flex flex-col'>
+        <form onSubmit={handleSubmit} className='gap-4 flex flex-col'>
             <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                <TextInput label='Title' name='title' type='text' placeholder='Enter title' required id='title' className='flex-1 ' />
-                <Select label='Category' name='category' id='category' required className='flex-1'>
+                <TextInput label='Title' name='title' type='text' placeholder='Enter title' required id='title' className='flex-1 ' 
+                    onChange={(e) => setFormData({...formData, title: e.target.value})}
+                    
+                    />
+                <Select onChange={(e) => setFormData({...formData, category: e.target.value})}
+                 label='Category' name='category' id='category' required className='flex-1'>
                     <option value='uncategorized'>Select a category</option>
                     <option value='javascript'>Javascript</option>
                     <option value='reactjs'>React.js</option>
@@ -85,8 +116,13 @@ export default function CreatePost() {
             {
                 formData.image && <img src={formData.image} alt='uploaded' className='w-full h-72 object-contain' />
             }
-            <ReactQuill className='h-72 mb-12 ' theme='snow' placeholder='Write something amazing...' required />
+            <ReactQuill className='h-72 mb-12 ' theme='snow' placeholder='Write something amazing...' required onChange={
+                (value) => setFormData({...formData, content: value})
+            } />
             <Button type='submit' gradientDuoTone='purpleToPink' size='lg' outline>Create Post</Button>
+            {
+                publishError && <Alert className='mt-5' color='failure'>{publishError}</Alert>
+            }
         </form>
     </div>
   )
